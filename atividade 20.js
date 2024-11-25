4946e8a7ed2b8a98e0f8b63cbabb68d98beda05c
scarllat2005 <162135041+scarllat2005@users.noreply.github.com>
2024-11-25 19:41:30 -0300

    Create atividade 20.js

---
+++ atividade 20.js
@@ -0,0 +1,7 @@
+function verificarDivisivel(num) {
+    if (num % 3 === 0 && num % 5 === 0) {
+        console.log("O número é divisível por 3 e 5.");
+    } else {
+        console.log("O número não é divisível por 3 e 5.");
+    }
+}
