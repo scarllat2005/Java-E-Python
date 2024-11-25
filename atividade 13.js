66db5995d09a5f552df49a1e94c89bafd665ca83
scarllat2005 <162135041+scarllat2005@users.noreply.github.com>
2024-11-25 19:32:23 -0300

    Create atividade 13.js

---
+++ atividade 13.js
@@ -0,0 +1,9 @@
+function compararNumeros(a, b) {
+    if (a > b) {
+        console.log("O primeiro número é maior que o segundo.");
+    } else if (a < b) {
+        console.log("O primeiro número é menor que o segundo.");
+    } else {
+        console.log("Os números são iguais.");
+    }
+}
