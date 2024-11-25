312ad393bc7fe268670f26a80af1c3835467ae87
scarllat2005 <162135041+scarllat2005@users.noreply.github.com>
2024-11-25 19:37:15 -0300

    Create atividade 17.js

---
+++ atividade 17.js
@@ -0,0 +1,7 @@
+function verificarIntervalo(num) {
+    if (num >= 10 && num <= 20) {
+        console.log("O número está dentro do intervalo.");
+    } else {
+        console.log("O número está fora do intervalo.");
+    }
+}
