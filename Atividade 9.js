42e33f31a2c898e780079c509ed9eb774020f28e
scarllat2005 <162135041+scarllat2005@users.noreply.github.com>
2024-11-25 19:27:32 -0300

    Create Atividade 9.js

---
+++ Atividade 9.js
@@ -0,0 +1,7 @@
+let numeros = [10, 5, 8, 23, 1];
+
+let maior = Math.max(...numeros);
+let menor = Math.min(...numeros);
+
+console.log("Maior número: " + maior);
+console.log("Menor número: " + menor);
