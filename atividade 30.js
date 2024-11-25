8c278c64bc6fdbcf5ae14dbf391be4d2e4beb38c
scarllat2005 <162135041+scarllat2005@users.noreply.github.com>
2024-11-25 19:50:03 -0300

    Create atividade 30.js

---
+++ atividade 30.js
@@ -0,0 +1,7 @@
+function verificarArrayVazio(array) {
+    if (array.length > 0) {
+        console.log("O array não está vazio.");
+    } else {
+        console.log("O array está vazio.");
+    }
+}
