d7bcf828ec9ac0b1bc0f27c8a5f772de5392883f
scarllat2005 <162135041+scarllat2005@users.noreply.github.com>
2024-11-25 19:35:20 -0300

    Create atividade 16.js

---
+++ atividade 16.js
@@ -0,0 +1,7 @@
+function verificarStringVazia(str) {
+    if (str.length === 0) {
+        console.log("A string está vazia.");
+    } else {
+        console.log("A string não está vazia.");
+    }
+}
