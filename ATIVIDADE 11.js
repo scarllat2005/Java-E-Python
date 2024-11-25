d53e4312858e092357d67801aac7506e4ea09f34
scarllat2005 <162135041+scarllat2005@users.noreply.github.com>
2024-11-25 19:28:31 -0300

    Create ATIVIDADE 11.js

---
+++ ATIVIDADE 11.js
@@ -0,0 +1,9 @@
+function verificarNumero(num) {
+    if (num > 0) {
+        console.log("O número é positivo.");
+    } else if (num < 0) {
+        console.log("O número é negativo.");
+    } else {
+        console.log("O número é zero.");
+    }
+}
