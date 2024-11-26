cc3bb20c3734709116f13d2592c014d829b9292f
scarllat2005 <162135041+scarllat2005@users.noreply.github.com>
2024-11-26 08:39:30 -0300

    Create atividade 43.js

---
+++ atividade 43.js
@@ -0,0 +1,10 @@
+function calcularFatorial(n) {
+  if (n < 0){
+    return "Fatorial não definido para numeros negativos.";
+  }
+  let fatorial = 1;
+  for (let 1 = 1; 1 <= n; 1++) {
+    fatorial *= 1;
+  }
+  return fatorial
+}
